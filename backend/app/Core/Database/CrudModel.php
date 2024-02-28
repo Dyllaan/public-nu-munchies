8cbb41644d68b466ee5890710be2420883e596c8
Filip Brebera <filip@bxxf.dev>
2024-02-28 04:34:03 +0000

    fix: add static type to DB, allows autocompletion in vscode
    when using  $this->getDb()->… now autocompletion works

--- backend/app/Core/Database/CrudModel.php
+++ backend/app/Core/Database/CrudModel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Crud Model gives the common functions
  * @auther Louis Figes
@@ -9,32 +10,33 @@ namespace Core\Database;
 
 use Core\HTTP\Classes\GivesResponse;
 
-class CrudModel extends GivesResponse {
+class CrudModel extends GivesResponse
+{
 
-    private $db;
+    private Database $db;
     private $id;
-    
-    public function __construct($db) 
+
+    public function __construct($db)
     {
         $this->db = $db;
     }
 
-    public function getId() 
+    public function getId()
     {
         return $this->id;
     }
 
-    public function setId($id) 
+    public function setId($id)
     {
         $this->id = $id;
     }
 
-    protected function getDb() 
+    protected function getDb()
     {
         return $this->db;
     }
 
-    private function setDb($db) 
+    private function setDb($db)
     {
         $this->db = $db;
     }

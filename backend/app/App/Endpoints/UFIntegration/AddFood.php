db6e17e740f74d17c2a52fd8dbd298b0cda03126
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-03 19:41:23 +0000

    Create AddFood.php

---
+++ backend/app/App/Endpoints/UFIntegration/AddFood.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * class AddFood
+     * @author Nathan Nordstrom-Hearne w21025072
+     * 
+     * This class is used to add the foods nutrients such as calories,
+     * weight, salt, fat, protein, etc. This will allow the user to be 
+     * able to track the exact amount of food that they are consuming and
+     * the amount of nutrients that they are also consuming.
+     */
+
+     namespace App\Endpoints\UFInegration
+?>

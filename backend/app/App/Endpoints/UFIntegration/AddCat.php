e83349aecc7b175e6b3b64e6b91752040de9efad
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-03 19:41:20 +0000

    Create AddCat.php

---
+++ backend/app/App/Endpoints/UFIntegration/AddCat.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     * class AddCat
+     * @author Nathan Nordstrom-Hearne w21025072
+     * 
+     * Used to upload a catagory to the cat database and to display 
+     * on the website.
+     */
+
+     namespace App\Endpoints\UFIntegration;
+
+    use Core\Endpoint\Endpoint;
+    use Core\HTTP\Classes\Request;
+    use Core\Database\Queries;
+    use Core\Database;
+
+    class AddCat extends Endpoint
+    {
+        public function _construct()
+        {
+            parent::_construct('POST', 'category');
+            $this->setRequiresAuth(true);
+            $this->getAttributes()->addRequiredString(['cat_name', 'cat_image']);
+        }
+
+        public function process($request)
+        {
+            parent::process($request);
+            $cat = Cat::getInstance($this->getDb());
+            $cat->setCatName($request->getAttribute('cat_name'));
+            $cat->setCatImage($request->getAttribute('cat_image'));
+            $cat->category($this->getDb());
+            $this->setResponse(200, 'Category Created', $cat->toArray());
+        }
+    }
+?>

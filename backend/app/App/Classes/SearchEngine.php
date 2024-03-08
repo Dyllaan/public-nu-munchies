503dcea33c746b0ca8b33adcaffdafa9fdbc1d29
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-08 10:01:37 +0000

    Create SearchEngine.php

---
+++ backend/app/App/Classes/SearchEngine.php
@@ -0,0 +1,43 @@
+<?php
+   /**
+     * class SeachEngine
+     * @author Nathan Nordstrom-Hearne w21025072
+     * 
+     * SearchEngine is used for implementing a search bar functionality.
+     */
+    namespace App\Classes;
+
+    use Firebase\JWT\JWT;
+    use Core\Database\CrudModel;
+    use Core\Database\CrudInterface;
+
+    class SearchEngine extends CrudModel implements CrudInterface
+    {
+        private $data;
+
+        private \AppConfig $appConfigInstance;
+
+        public function __construct($db)
+        {
+            parent::_construct($db);
+            $this->appConfigInstance = new \Appconfig();
+        }
+
+        public function search($query)
+        {
+            $conn = $this->db->connect();
+            $stmt = $conn->prepare("SELECT * FROM categories WHERE cat_name LIKE :query");
+            $query = "%$query%";
+            $stmt->bindParam(' :query', $query);
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        }
+    }
+
+    /**
+     * $searchEngine = new SearchEngine();
+     * $results = $searchEngine->search("search_query");
+     * print_r($results);
+     */
+?>

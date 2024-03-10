6fb863d5b2a1b98df7d7303f89acd9e0955e164a
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-10 16:23:21 +0000

    Create CatEndpoint.php

---
+++ backend/app/App/Endpoints/CatEndpoint.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * class CatEndpoint
+     * @author Nathan Nordstrom-Hearne w21025072
+     * 
+     * Allows the added, edit, remove to a specific category.
+     */
+    namespace App\Endpoints;
+
+    use App\Classes\Category;
+    use \App\Endpoints\UFIntegration\AddCat;
+    use \App\Endpoints\UFIntegration\EditCat;
+    use \App\Endpoints\UFIntegration\RemoveCat;
+
+    class CatEndpoint extends Endpoint
+    {
+        public function __construct()
+        {
+            parent::__construct('GET', 'category');
+            $this->addSubEndpoint(new AddCat());
+            $this->addSubEndpoint(new EditCat());
+            $this->addSubEndpoint(new RemoveCat());
+        }
+        public function process($request)
+        {
+            parent::process($request);
+            $this->setResponse(200, 'Category Taken', $this->getCategory()->toArray());
+        }
+    }
+?>

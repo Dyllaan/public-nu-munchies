01ec0e6ae3e1f2d37942dc1496698fd038949d73
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-12 13:58:14 +0000

    Update page.tsx

--- frontend/app/(food-subsystem)/category/page.tsx
+++ frontend/app/(food-subsystem)/category/page.tsx
@@ -1,1 +1,23 @@
-"use client";
+
+import category from '../components/CategoryList'
+
+
+import React from 'react';
+
+const categories = ['example1', 'example2', 'example3', 'example4'];
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h2>Categories</h2>
+            <ul>
+                {categories.map((cat, index) => (
+                    <li key={index}>{cat}</li>
+                ))}
+            </ul>
+            <p>Hello world</p> {/* This should be inside the div */}
+        </div>
+    );
+};
+
+export default CategoryList;

4bdb838749b361809f72825bf3f442ff9e24e071
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-14 20:44:03 +0000

    Update EditCat.php

--- backend/app/App/Endpoints/UFIntegration/EditCat.php
+++ backend/app/App/Endpoints/UFIntegration/EditCat.php
@@ -10,6 +10,7 @@ class EditCat extends SubEndpoint
     public function __construct()
     {
         parent::__construct('PUT', 'editcategory');
+        $this->getAttributes()->addRequiredInts(['cat_id']);
         $this->getAttributes()->addAllowedStrings(['cat_name', 'cat_image']);
     }
 
@@ -17,6 +18,12 @@ class EditCat extends SubEndpoint
     {
         parent::process($request);
         $changeFlag = false;
+
+        $cat = new Category($this->getDb());
+
+        $cat_id = $request->getAttribute('cat_id');
+
+        $cat->setCatId($cat_id);
 
         // Check if $request is an array or an object with the necessary methods
         if (!is_array($request) && !method_exists($request, 'getAttributes')) {
@@ -31,18 +38,18 @@ class EditCat extends SubEndpoint
             return;
         }
 
-        if ($request->hadAttribute('cat_name')) {
+        if ($request->hasAttribute('cat_name')) {
             $changeFlag = true;
-            $this->getCategory()->setCatName($request->getAttribute('cat_name'));
+            $cat->setCatName($request->getAttribute('cat_name'));
         }
-        if ($request->hadAttribute('cat_image')) {
+        if ($request->hasAttribute('cat_image')) {
             $changeFlag = true;
-            $this->getCategory()->setCatImage($request->getAttribute('cat_image'));
+            $cat->setCatImage($request->getAttribute('cat_image'));
         }
 
         if ($changeFlag) {
-            $this->getCategory()->update();
-            $this->setResponse(201, "Category Updated", $this->getCategory()->toArray());
+            $cat->update();
+            $this->setResponse(201, "Category Updated", $cat->toArray());
         } else {
             $this->setResponse(400, ['message' => 'No attributes to edit']);
         }

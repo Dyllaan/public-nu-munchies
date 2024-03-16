c45d822d99eb326b1755d7af440bc6849a07c6e5
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-16 12:20:19 +0000

    Update RemoveNutrition.php

--- backend/app/App/Endpoints/UFIntegration/RemoveNutrition.php
+++ backend/app/App/Endpoints/UFIntegration/RemoveNutrition.php
@@ -15,15 +15,16 @@
         public function __construct()
         {
             parent::__construct('DELETE', 'removenutrition');
-            $this->setRequiresAuth(true);
             $this->getAttributes()->addRequiredInts(['food_id']);
         }
         public function process($request)
         {
-            $nutrition = ['nutrition' => 'removed'];
-            $id = $this->getDb()->createDelete()->table('nutrition_details')->set($nutrition)->where("id - '" . $request->getAttribute('food_id'). "'")->execute();
-
-            $this->setResponse(200, 'Foods Nutrition Removed', ['id' => $id]);
+            parent::process($request);
+            $nutrition = new Nutrition($this->getDb());
+            $food_id = $request->getAttribute('food_id');
+            $nutrition->setFoodId($food_id);
+            $nutritionData = $nutrition->delete($food_id);
+            $this->setResponse(200, 'Nutrition Deleted', $nutritionData);
         }
      }
 ?>

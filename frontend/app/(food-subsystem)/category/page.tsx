ac1277711e8111ea7a0bdd90d443c297cf2acd4d
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-16 14:07:46 +0000

    Update page.tsx

--- frontend/app/(food-subsystem)/category/page.tsx
+++ frontend/app/(food-subsystem)/category/page.tsx
@@ -1,37 +1,13 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import Category from '../components/Category'
 
-
-function Items() {
-  const [category, setCategory] = useState([]);
-  
-  const fetchData = () => { 
-    fetch("http://localhost:8080/getcategories")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(json => setCategory(json))
-      .catch(error => {
-        console.error('Error fetching data:', error); 
-      });
-  };
-
-  useEffect(fetchData, []);
-
-  const itemsDisplay = (
-    <div className="bg-[#eaeaea] my-2 rounded">
-      {category.map((category, key) => (
-        <div key={key} className="mb-2">
-          <p className="font-bold">{category.cat_name}</p>
-        </div>
-      ))}
-    </div>
-  );
-
+function Main() {
+  return (
+    <>
+      <Category />
+    </>
+  )
 }
 
-export default Items;
+export default Main

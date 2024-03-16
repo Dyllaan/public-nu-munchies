4d1aacc07f527010d5fa3451e7035b9becf95590
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-16 21:16:21 +0000

    Update page.tsx

--- frontend/app/(food-subsystem)/searchBar/page.tsx
+++ frontend/app/(food-subsystem)/searchBar/page.tsx
@@ -10,7 +10,6 @@ function App()
         <div className="App">
             <div className="search-bar-container">
                 <SearchBar />
-                <div>SearchResults</div>
             </div>
         </div>
     );

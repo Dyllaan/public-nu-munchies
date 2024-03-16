f767e8eddc1c47d62be86baf2c6096e8731410b9
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-16 13:35:39 +0000

    Update page.tsx

--- frontend/app/(food-subsystem)/category/page.tsx
+++ frontend/app/(food-subsystem)/category/page.tsx
@@ -1,64 +1,37 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import * as api from '@/hooks/user-subsystem/api'
 
-function Category() {
-    const [category, setCategory] = useState<{ data: any[] }>({
-        data: []
-    });
-    const [selectedCategory, setSelectedCategory] = useState<any>(null);
 
-    const fetchData = async () => {
-        const res = await api.get("category", localStorage.getItem("token"));
-        if(res.success)
-        {
-            setCatState(res.data.data.category);
-            setLoading(false);
-        } else {
-            console.error("Failed to get user:", res.data.message);
-            setLoading(false);
+function Items() {
+  const [category, setCategory] = useState([]);
+  
+  const fetchData = () => { 
+    fetch("http://localhost:8080/getcategories")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-        return res;
-    };
+        return response.json();
+      })
+      .then(json => setCategory(json))
+      .catch(error => {
+        console.error('Error fetching data:', error); 
+      });
+  };
 
-    useEffect(() => {
-        if (category.data.length > 0) return;
+  useEffect(fetchData, []);
 
-        fetchData().then(res => {
-            setCategory(res);
-        });
-    }, [category]);
-
-    const handleClick = (item: any) => {
-        setSelectedCategory(item);
-    }
-
-    const categoryDisplay = (
-        <div>
-            <div>
-                {category.data?.map((value: any, key: number) => (
-                    <div key={key} className="mb-2" onClick={() => handleClick(value)}>
-                        <p>Category: {value.cat_name}</p>
-                    </div>
-                ))}
-            </div>
+  const itemsDisplay = (
+    <div className="bg-[#eaeaea] my-2 rounded">
+      {category.map((category, key) => (
+        <div key={key} className="mb-2">
+          <p className="font-bold">{category.cat_name}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 
-    return (
-        <>
-            {categoryDisplay}
-        </>
-    )
 }
 
-export default Category;
-
-function setCatState(category: any) {
-    throw new Error('Function not implemented.');
-}
-function setLoading(arg0: boolean) {
-    throw new Error('Function not implemented.');
-}
-
+export default Items;

196dfd08ceb07c2f4fe220f77c7b0488536ea699
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-17 11:16:44 +0000

    Update NutritionResults.tsx

--- frontend/app/(food-subsystem)/components/NutritionResults.tsx
+++ frontend/app/(food-subsystem)/components/NutritionResults.tsx
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import {useState, useEffect} from 'react';
-import { useLocation } from 'react-router-dom';
-import "../css/food.css";
+import { useLocation } from 'react-router-dom'
 import { Input } from "@/components/ui/input";
+import {atom, useAtom} from "jotai";
 import {useRouter} from 'next/navigation';
-import useFetchData from '@/hooks/user-subsystem/useFetchData';
 import {number, z} from "zod"; 
+
+import LoadingInPage from '@/app/(user-subsystem)/components/LoadingInPage';
+import useFetchData from '@/hooks/user-subsystem/useFetchData';
+import "../css/food.css";
 
 interface Nutrition{
   food_id?: number
@@ -26,8 +29,40 @@ function Nutritions(){
   const [nutritions, setNutritions] = useState<{data: Nutrition[]}> ({
     data: []
   });
+  const [selectedNutrition, setSelectedNutrition] = useAtom(selectedNutritionAtom);
   
+  const {data, loading} = useFetchData("addnutrition");
+
+  const handleClick = (nutrition: Nutrition) => {
+    console.log(nutrition);
+    setSelectedNutrition(nutrition);
+  }
+  if(loading){
+    return <LoadingInPage />
+  }
+  return (
+    <>
+      <div>
+      {data.map((value: any, key) => (
+        <div key={key} >
+          <h2>Submitted Data</h2>
+          <p>Name: {value.food_name}</p>
+          <p>Weight: {value.weight}</p>
+          <p>Calories: {value.calories}</p>
+          <p>Protein: {value.protein}</p>
+          <p>Carbohydrates: {value.carbs}</p>
+          <p>Fat: {value.fat}</p>
+          <p>Salt: {value.salt}</p>
+          <p>Quantity: {value.quantity}</p>
+        </div>
+      ))}
+      
+    </div>
+    </>
+  )
 }
+export const selectedNutritionAtom = atom<Nutrition>({food_id: undefined, food_name: undefined, weight: undefined, calories:undefined, protein: undefined, carbs: undefined, fat: undefined, salt:undefined, quantity: undefined});
+export default Nutritions;
 
 
 /*

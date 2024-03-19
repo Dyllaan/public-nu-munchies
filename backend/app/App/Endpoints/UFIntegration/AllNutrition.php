f12f8bcc088c9fb7c5e32172554c0353f13f61d1
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-19 13:07:19 +0000

    Create AllNutrition.php

---
+++ backend/app/App/Endpoints/UFIntegration/AllNutrition.php
@@ -0,0 +1,37 @@
+<?php
+
+    namespace App\Endpoints\UFIntegration;
+
+    use Core\Endpoint\Endpoint;
+    use App\Classes\Nutrition;
+
+    class GetNutrition extends Endpoint
+    {
+        public function __construct()
+        {
+            parent::__construct('GET', 'allnutrition');
+            $this->getAttributes()->addRequiredInt('item_id');
+        }
+        public function process($request)
+        {
+            parent::process($request);
+
+            $nutrition = new Nutrition($this->getDb());
+
+            /*
+            $item_id = $request->getAttribute('item_id');
+            if(strlen($item_id) > 0){
+                $nutrition->setItemId($item_id);
+                $nutritionData = $nutrition->get($item_id);
+                $nutritionArray = $nutrition->toArray();
+
+                $this->setResponse(200, 'Nutrition Shown', $nutritionArray);
+            }
+            $this->setResponse(200, 'No Data', []);
+            */            
+
+           $this->setResponse(200, 'All Nutrition Shown', $nutrition->getAll());
+        }
+    }
+
+?>

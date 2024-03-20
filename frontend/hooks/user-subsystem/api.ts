50eb85fb08f9a7016ac0425290b710bcfd7b2e37
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-03-20 12:55:12 +0000

    Update api.ts

--- frontend/hooks/user-subsystem/api.ts
+++ frontend/hooks/user-subsystem/api.ts
@@ -1,5 +1,5 @@
 "use-client";
-const BASE_URL = 'https://backend.nu-munchies.xyz/';
+const BASE_URL = 'http://localhost:8080/';
 
 async function handleResponse(response:any) {
     const contentType = response.headers.get("content-type");

3b643b91f69d411e4adf2e4be25a939fb6c7a17e
Louis <38404111+Dyllaan@users.noreply.github.com>
2024-03-21 20:54:47 +0000

    Update Header.tsx

--- frontend/app/layout-components/Header.tsx
+++ frontend/app/layout-components/Header.tsx
@@ -1,28 +1,35 @@
 "use client";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import MobileHeader from "./MobileHeader";
 import DesktopHeader from "./header-types/DesktopHeader";
+import { useEffect, useState } from "react";
 
 /**
  * @author Louis Figes W21017657
  */
 
 export default function Header() {
-  
+  const [isMobile, setIsMobile] = useState(false);
+
   /**
   * Arbitrary breakpoint for mobile 
   * Suggested by: https://testsigma.com/blog/css-breakpoints/
   * I know this is not a good source
   */
-  const isMobile = window.innerWidth < 480;
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 480);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   
   return (
     <NavigationMenu>
